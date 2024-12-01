fa4e4acfc36cef5922f68155143910d4fafc1bfb
Yousei <youseifs422@outlook.com>
2024-12-01 19:54:42 +0900

    Add Makefile

--- ch3/4_pid.c
+++ ch3/4_pid.c
@@ -1,22 +1,7 @@
 /**
- * hello.c
- *
  * Kernel module that communicates with /proc file system.
- * 
- * @note
- * (1) The makefile must be modified to compile this program.
- * Add "2_hello.o" to the [obj-m] variable so the compiler knows what to build.
  *
- * The /proc file system is a "pseudo" file system that exists only in kernel memory,
- * and is used primarily for querying various kernel and per-process statistics.
- * 
- * (2) After loaded the kernel module, enters the command
- * [cat /proc/hello]
- * 
- * the infamous Hello World message is returned.
- * 
- * Operating System Concepts - 10th Edition
- * Copyright John Wiley & Sons - 2018
+ * This provides the base logic for Project 2 - displaying task information
  */
 
 /* Header */
@@ -26,6 +11,9 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/proc_fs.h>
+#include <linux/sched.h>
+#include <linux/slab.h>
+#include <linux/vmalloc.h>
 
 /* Macro */
 
@@ -34,25 +22,23 @@
 #define EXIT_SUCCESS 0 /* Successful exit status */
 
 #define BUFFER_SIZE  (128u)
-
-#define PROC_NAME    "hello"
-#define MESSAGE      "Hello World\n"
+#define PROC_NAME    "pid"
 
 /* Type */
 
 /* Prototype */
 
 static ssize_t proc_read(struct file* file, char* buf, size_t count, loff_t* pos);
+static ssize_t proc_write(struct file* file, const char __user* usr_buf, size_t count, loff_t* pos);
 
 /* Variable */
 
-/** 
- * It contains a reference to a struct /proc filesystem operations. This struct initializes the .proc_flags and .proc_read members. 
- * The value of .proc_read is the name of the function proc_read() that is to be called whenever /proc/hello is read.
- */
-static struct proc_ops proc_ops = {
-    .proc_flags = 0,
-    .proc_read  = proc_read,
+/** The current pid */
+static long l_pid;
+
+static struct file_operations proc_ops = {
+    .owner = THIS_MODULE,
+    .read  = proc_read,
 };
 
 /* Function */
@@ -61,10 +47,10 @@ static struct proc_ops proc_ops = {
 static int proc_init(void)
 {
     /**
-     * Creates the /proc/hello entry
+     * Creates the /proc/pid entry
      * The following function call is a wrapper for proc_create_data() passing NULL as the last argument
      */
-    proc_create(PROC_NAME, 0, NULL, &proc_ops);
+    proc_create(PROC_NAME, 0666, NULL, &proc_ops);
 
     printk(KERN_INFO "/proc/%s created\n", PROC_NAME);
 
@@ -74,7 +60,7 @@ static int proc_init(void)
 /** This function is called when the module is removed */
 static void proc_exit(void)
 {
-    // Removes the /proc/hello entry
+    // Removes the /proc/pid entry
     remove_proc_entry(PROC_NAME, NULL);
 
     printk(KERN_INFO "/proc/%s removed\n", PROC_NAME);
@@ -82,7 +68,7 @@ static void proc_exit(void)
 
 /**
  * @brief
- * This function is called each time the /proc/hello is read.
+ * This function is called each time the /proc/pid is read.
  * 
  * This function is called repeatedly until it returns 0, so there must be logic that ensures it ultimately returns 0 once it has collected the data that is to go into the corresponding /proc file.
  *
@@ -93,8 +79,10 @@ static void proc_exit(void)
  */
 static ssize_t proc_read(struct file* file, char __user* usr_buf, size_t count, loff_t* pos)
 {
+    int rv = 0;
     char buffer[BUFFER_SIZE];
-    static int completed = 0;
+    static int completed    = 0;
+    struct task_struct* tsk = NULL;
 
     if (completed)
     {
@@ -102,27 +90,51 @@ static ssize_t proc_read(struct file* file, char __user* usr_buf, size_t count, 
         return 0;
     }
 
-    completed                     = 1;
+    tsk       = pid_task(find_vpid(l_pid), PIDTYPE_PID);
 
-    // The string "Hello World∖n" is written to the variable buffer where buffer exists in kernel memory.
-    int numberOfCharactersWritten = sprintf(buffer, MESSAGE);
+    completed = 1;
 
-    printk(KERN_INFO "numberOfCharactersWritten is %d\n", numberOfCharactersWritten);
-
-    // Copies the contents of kernel memory buffer to to the variable usr_buf, which exists in user space.
-    const unsigned int error = copy_to_user(usr_buf, buffer, numberOfCharactersWritten);
-
-    if (error)
+    // Copies the contents of kernel buffer to userspace usr_buf
+    if (copy_to_user(usr_buf, buffer, rv))
     {
-        printk(KERN_INFO "Copy Error: error code is %u\n", error);
+        rv = -1;
     }
 
-    return numberOfCharactersWritten;
+    return rv;
+}
+
+/**
+ * This function is called each time we write to the /proc file system.
+ */
+static ssize_t proc_write(struct file* file, const char __user* usr_buf, size_t count, loff_t* pos)
+{
+    char* k_mem;
+
+    // allocate kernel memory
+    k_mem = kmalloc(count, GFP_KERNEL);
+
+    /* copies user space usr_buf to kernel buffer */
+    if (copy_from_user(k_mem, usr_buf, count))
+    {
+        printk(KERN_INFO "Error copying from user\n");
+        return -1;
+    }
+
+    /**
+ 	 * kstrol() will not work because the strings are not guaranteed
+	 * to be null-terminated.
+	 * 
+	 * sscanf() must be used instead.
+	 */
+
+    kfree(k_mem);
+
+    return count;
 }
 
 /* Kernel module Setting */
 
-/** Macros for registering module entry and exit points */
+/* Macros for registering module entry and exit points */
 module_init(proc_init); // The module entry point, which represents the function that is invoked when the module is loaded into the kernel
 module_exit(proc_exit); // The module exit point, which represents the function that is called when the module is removed from the kernel
 
@@ -133,5 +145,5 @@ module_exit(proc_exit); // The module exit point, which represents the function 
  * (3) Author
  */
 MODULE_LICENSE("GPL");
-MODULE_DESCRIPTION("Hello Module");
+MODULE_DESCRIPTION("PID Module");
 MODULE_AUTHOR("SGG");
--- ch3/newproc-win32.c
+++ ch3/newproc-win32.c
@@ -7,41 +7,40 @@
  * Operating System Concepts  - Tenth Edition
  * Copyright John Wiley & Sons - 2018
  */
- 
-#include <windows.h>
-#include <stdio.h>
 
-int main( VOID )
+#include <stdio.h>
+#include <windows.h>
+
+int main(VOID)
 {
     STARTUPINFO si;
     PROCESS_INFORMATION pi;
 
-    ZeroMemory( &si, sizeof(si) );
+    ZeroMemory(&si, sizeof(si));
     si.cb = sizeof(si);
-    ZeroMemory( &pi, sizeof(pi) );
+    ZeroMemory(&pi, sizeof(pi));
 
     // Start the child process.
-    if( !CreateProcess( NULL,   // No module name (use command line).
-        "C:\\WINDOWS\\system32\\mspaint.exe", // Command line.
-        NULL,             // Process handle not inheritable.
-        NULL,             // Thread handle not inheritable.
-        FALSE,            // Set handle inheritance to FALSE.
-        0,                // No creation flags.
-        NULL,             // Use parent's environment block.
-        NULL,             // Use parent's starting directory.
-        &si,              // Pointer to STARTUPINFO structure.
-        &pi )             // Pointer to PROCESS_INFORMATION structure.
+    if (! CreateProcess(NULL,                     // No module name (use command line).
+            "C:\\WINDOWS\\system32\\mspaint.exe", // Command line.
+            NULL,                                 // Process handle not inheritable.
+            NULL,                                 // Thread handle not inheritable.
+            FALSE,                                // Set handle inheritance to FALSE.
+            0,                                    // No creation flags.
+            NULL,                                 // Use parent's environment block.
+            NULL,                                 // Use parent's starting directory.
+            &si,                                  // Pointer to STARTUPINFO structure.
+            &pi)                                  // Pointer to PROCESS_INFORMATION structure.
     )
     {
-        printf( "CreateProcess failed (%d).\n", GetLastError() );
+        printf("CreateProcess failed (%d).\n", GetLastError());
         return -1;
     }
 
     // Wait until child process exits.
-    WaitForSingleObject( pi.hProcess, INFINITE );
+    WaitForSingleObject(pi.hProcess, INFINITE);
 
     // Close process and thread handles.
-    CloseHandle( pi.hProcess );
-    CloseHandle( pi.hThread );
+    CloseHandle(pi.hProcess);
+    CloseHandle(pi.hThread);
 }
-
